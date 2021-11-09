4054cf1d432d127278b808e1bd797f491044c81c
Zlatan Gavranovic <zlatangavranovic1906@gmail.com>
2021-11-09 18:52:22 +0100

    adding logic and design

--- mp3.js
+++ mp3.js
@@ -1,28 +1,33 @@
-const preSong = document.getElementById('repFile');
-const lastSong = document.getElementById('lastFile');
+const preSong = document.getElementById('preFile');
+const lastSong = document.getElementById('nextFile');
 const audio =  document.getElementById('audio');
+const body = document.getElementsByTagName('body')[0];
+const play = document.getElementById('play');
 
-let songIndex = 0;
+let songIndex = 1; console.log(songIndex)
 
-const songs = ['Kalimba','MaidwiththeFlaxenHair','SleepAway'];
+const songs = ['Kalimba','Maid with the Flaxen Hair','Sleep Away'];
+const bgColor = ['#C89D7C','#ADD8E6','#FFD580'];
 
 
 loadSong(songs[songIndex]);
 
 function loadSong(song){
     audio.src = `audio/${song}.mp3`;
+    play.innerHTML = songs[songIndex];
+    play.style.background = bgColor[songIndex];
+    play.style.color = bgColor[songIndex];
+    body.style.background = bgColor[songIndex];
+    body.style.transition = '1s';
 }
 
-/* preSong.addEventListener('click', () => {
-    songIndex++
-    if( songIndex > songs.length - 1){
-        songIndex = 0;
-    }
-})
- */
+preSong.addEventListener('click', prevSong);
+lastSong.addEventListener('click', nextSong);
+
+
 
 // Previous song
-function preSong() {
+function prevSong() {
     songIndex--;
   
     if (songIndex < 0) {
@@ -30,6 +35,8 @@ function preSong() {
     }
   
     loadSong(songs[songIndex]);
+
+    audio.play();
   
   }
   
@@ -42,5 +49,7 @@ function preSong() {
     }
   
     loadSong(songs[songIndex]);
+
+    audio.play();
   
   }
