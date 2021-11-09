07e1193565b6ab1142b70f14b47010d0044c604e
Zlatan Gavranovic <zlatangavranovic1906@gmail.com>
2021-11-09 22:04:53 +0100

    responsive design and details

--- mp3.js
+++ mp3.js
@@ -4,26 +4,35 @@ const audio =  document.getElementById('audio');
 const body = document.getElementsByTagName('body')[0];
 const play = document.getElementById('play');
 
-let songIndex = 1; console.log(songIndex)
+let songIndex = 1;
+
+// song titles array
 
 const songs = ['Kalimba','Maid with the Flaxen Hair','Sleep Away'];
+
+// font and background color arrays
+
 const bgColor = ['#C89D7C','#ADD8E6','#FFD580'];
+const fontColor = ['#FBD0AF','#E0FFFF','#FFFFB3'];
 
 
 loadSong(songs[songIndex]);
+
+//  The loadLong function generates the song title, the background and font color and also the transition
 
 function loadSong(song){
     audio.src = `audio/${song}.mp3`;
     play.innerHTML = songs[songIndex];
     play.style.background = bgColor[songIndex];
-    play.style.color = bgColor[songIndex];
+    play.style.color = fontColor[songIndex];
     body.style.background = bgColor[songIndex];
     body.style.transition = '1s';
 }
 
+// Event listener for previous and next song
+
 preSong.addEventListener('click', prevSong);
 lastSong.addEventListener('click', nextSong);
-
 
 
 // Previous song
@@ -36,8 +45,10 @@ function prevSong() {
   
     loadSong(songs[songIndex]);
 
-    audio.play();
-  
+    setTimeout(() => {
+      audio.play();
+    }, 500);
+    
   }
   
   // Next song
@@ -50,6 +61,8 @@ function prevSong() {
   
     loadSong(songs[songIndex]);
 
-    audio.play();
+    setTimeout(() => {
+      audio.play();
+    }, 500);
   
   }
