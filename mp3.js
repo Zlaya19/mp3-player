9df4ebb8d4b160fd5c01bda41e6e5fb1032e6600
Zlatan Gavranovic <zlatangavranovic1906@gmail.com>
2021-11-23 20:33:43 +0100

    dark theme

--- mp3.js
+++ mp3.js
@@ -3,6 +3,10 @@ const lastSong = document.getElementById('nextFile');
 const audio =  document.getElementById('audio');
 const body = document.getElementsByTagName('body')[0];
 const play = document.getElementById('play');
+const btnTheme = document.getElementById('darkTheme');
+const container = document.getElementById('container');
+const topContainer = document.getElementById('topContainer');
+const spanHeadline = document.getElementById('spanHeadline');
 
 let songIndex = 0;
 
@@ -27,6 +31,8 @@ function loadSong(song){
     play.style.color = fontColor[songIndex];
     body.style.background = bgColor[songIndex];
     body.style.transition = '1s';
+    preSong.style.color = bgColor[songIndex];
+    lastSong.style.color = bgColor[songIndex];
 }
 
 // Event listener for previous and next song
@@ -66,3 +72,28 @@ function prevSong() {
     }, 500);
   
   }
+
+
+  btnTheme.addEventListener('click', () => {
+    
+    container.style.background = '#333'
+    container.style.boxShadow = '0 0 25px #111, inset 0 0 10px #111';
+    container.style.border = '5px solid #333';
+    container.style.transition = '1.5s';
+    topContainer.style.background = 'rgb(56,56,56)';
+    topContainer.style.boxShadow = '0 0 15px #111';
+    
+    play.style.border = '5px solid #444';
+    play.style.boxShadow = '0 0 15px #111, inset 0 0 8px #333';
+    
+    preSong.style.background = '#333';
+    preSong.style.border = '#111';
+    preSong.style.boxShadow = '0 0 15px #111';
+    
+    lastSong.style.border = '#111';
+    lastSong.style.background = '#333';
+    lastSong.style.boxShadow = '0 0 15px #111';
+    
+    audio.classList.add('currentPartDark');
+    spanHeadline.classList.add('spanDark');
+  });
