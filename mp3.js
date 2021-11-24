ab6f5096b32476fbd6423eed5d430b35e34d9121
Zlatan Gavranovic <zlatangavranovic1906@gmail.com>
2021-11-24 21:57:56 +0100

    comments added

--- mp3.js
+++ mp3.js
@@ -79,108 +79,115 @@ function prevSong() {
 
   btnTheme.addEventListener('click', () => {
 
-    if(statusTheme === false){
-      darkTheme();
-      btnTheme.innerText = '⛅️';
+/*  
 
-        preSong.addEventListener('mouseenter', () => {
-          preSong.style.background = '#222';
-        });
+  if statusTheme is false, the darkTheme function switch the container to the dark theme.
+  The innerText takes a new value (sun with cloud) and we add hover effects on the btn-s.
+  At the end, the statustheme varible will be set to true.
 
-        lastSong.addEventListener('mouseenter', () => {
-          lastSong.style.background = '#222';
-        });
+*/
 
-        preSong.addEventListener('mouseleave', () => {
-          preSong.style.background = '#333';
-        });
+if(statusTheme === false){
+  darkTheme();
+  btnTheme.innerText = '⛅️';
 
-        lastSong.addEventListener('mouseleave', () => {
-          lastSong.style.background = '#333';
-        });
+    preSong.addEventListener('mouseenter', () => {
+      preSong.style.background = '#222';
+    });
 
-      statusTheme = true;
-    }
-    else
-    {
-      lightTheme();
-      btnTheme.innerText = '🌙';
+    lastSong.addEventListener('mouseenter', () => {
+      lastSong.style.background = '#222';
+    });
 
-        preSong.addEventListener('mouseenter', () => {
-          preSong.style.background = '#ddd';
-        });
+    preSong.addEventListener('mouseleave', () => {
+      preSong.style.background = '#333';
+    });
 
-        lastSong.addEventListener('mouseenter', () => {
-          lastSong.style.background = '#ddd';
-        });
+    lastSong.addEventListener('mouseleave', () => {
+      lastSong.style.background = '#333';
+    });
 
-        preSong.addEventListener('mouseleave', () => {
-          preSong.style.background = '#eee';
-        });
+  statusTheme = true;
+}
+else
+{
 
-        lastSong.addEventListener('mouseleave', () => {
-          lastSong.style.background = '#eee';
-        });
+//  By statusTheme true, will activate the lightTheme function with the moon sign
 
-      statusTheme = false;
-    }
+  lightTheme();
+  btnTheme.innerText = '🌙';
 
-  });
+    preSong.addEventListener('mouseenter', () => {
+      preSong.style.background = '#ddd';
+    });
 
+    lastSong.addEventListener('mouseenter', () => {
+      lastSong.style.background = '#ddd';
+    });
+
+    preSong.addEventListener('mouseleave', () => {
+      preSong.style.background = '#eee';
+    });
+
+    lastSong.addEventListener('mouseleave', () => {
+      lastSong.style.background = '#eee';
+    });
+
+// after switching the theme, we set the statusTheme to false
+
+  statusTheme = false;
+}
+
+});
+
+//  the dark theme function
+
+function darkTheme(){
+
+  container.style.background = '#333'
+  container.style.boxShadow = '0 0 25px #111, inset 0 0 10px #111';
+  container.style.border = '5px solid #333';
+  container.style.transition = '1.5s';
+  topContainer.style.background = 'rgb(56,56,56)';
+  topContainer.style.boxShadow = '0 0 15px #111';
   
+  play.style.border = '5px solid #444';
+  play.style.boxShadow = '0 0 15px #111, inset 0 0 8px #333';
+  
+  preSong.style.background = '#333';
+  preSong.style.border = '3px solid #333';
+  preSong.style.boxShadow = '0 0 15px #111';
+  
+  lastSong.style.border = '3px solid #333';
+  lastSong.style.background = '#333';
+  lastSong.style.boxShadow = '0 0 15px #111';
+  
+  audio.classList.add('currentPartDark');
+  spanHeadline.style.textShadow = '0 0 3px #111';
+}
 
-  function darkTheme(){
+//  the light theme function
 
-/*     btnTheme.style.background = '#333';
-    btnTheme.style.border = '3px solid #333';
-    btnTheme.style.boxShadow = '0 0 5px #111'; */
+function lightTheme(){
 
-    container.style.background = '#333'
-    container.style.boxShadow = '0 0 25px #111, inset 0 0 10px #111';
-    container.style.border = '5px solid #333';
-    container.style.transition = '1.5s';
-    topContainer.style.background = 'rgb(56,56,56)';
-    topContainer.style.boxShadow = '0 0 15px #111';
-    
-    play.style.border = '5px solid #444';
-    play.style.boxShadow = '0 0 15px #111, inset 0 0 8px #333';
-    
-    preSong.style.background = '#333';
-    preSong.style.border = '3px solid #333';
-    preSong.style.boxShadow = '0 0 15px #111';
-    
-    lastSong.style.border = '3px solid #333';
-    lastSong.style.background = '#333';
-    lastSong.style.boxShadow = '0 0 15px #111';
-    
-    audio.classList.add('currentPartDark');
-    spanHeadline.style.textShadow = '0 0 3px #111';
-  }
-
-  function lightTheme(){
-/* 
-    btnTheme.style.background = '#fff';
-    btnTheme.style.border = '3px solid #eee';
-    btnTheme.style.boxShadow = '0 0 5px #aaa'; */
-
-    container.style.background = 'linear-gradient( #ccc, whitesmoke)'
-    container.style.boxShadow = '0 0 30px #999, inset 0 0 6px #999';
-    container.style.border = '5px solid #fff';
-    container.style.transition = '1.5s';
-    topContainer.style.background = 'linear-gradient( #ccc, whitesmoke)';
-    topContainer.style.boxShadow = '0 2px 14px #999, 0 2px 14px #999';
-    
-    play.style.border = '4px solid #eee';
-    play.style.boxShadow = '0 0 14px #777, inset 0 0 14px #777';
-    
-    preSong.style.background = '#fff';
-    preSong.style.border = '3px solid #eee';
-    preSong.style.boxShadow = '0 0 14px #aaa, 0 0 14px #aaa';
-    
-    lastSong.style.background = '#fff';
-    lastSong.style.border = '3px solid #eee';
-    lastSong.style.boxShadow = '0 0 14px #aaa, 0 0 14px #aaa';
-    
-    audio.classList.remove('currentPartDark');
-    spanHeadline.style.textShadow = '0 3px 5px #777, 0 3px 5px #777';
-  }
+  container.style.background = 'linear-gradient( #ccc, whitesmoke)'
+  container.style.boxShadow = '0 0 30px #999, inset 0 0 6px #999';
+  container.style.border = '5px solid #fff';
+  container.style.transition = '1.5s';
+  topContainer.style.background = 'linear-gradient( #ccc, whitesmoke)';
+  topContainer.style.boxShadow = '0 2px 14px #999, 0 2px 14px #999';
+  
+  play.style.border = '4px solid #eee';
+  play.style.boxShadow = '0 0 14px #777, inset 0 0 14px #777';
+  
+  preSong.style.background = '#fff';
+  preSong.style.border = '3px solid #eee';
+  preSong.style.boxShadow = '0 0 14px #aaa, 0 0 14px #aaa';
+  
+  lastSong.style.background = '#fff';
+  lastSong.style.border = '3px solid #eee';
+  lastSong.style.boxShadow = '0 0 14px #aaa, 0 0 14px #aaa';
+  
+  audio.classList.remove('currentPartDark');
+  spanHeadline.style.textShadow = '0 3px 5px #777, 0 3px 5px #777';
+}
